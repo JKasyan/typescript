83e7b83a82edfb3e1740b9a990517c38109d6586
JKasyan <j.kasyan86@gmail.com>
2017-08-09 00:12:43 +0300

    Functions

--- src/functions.ts
+++ src/functions.ts
@@ -2,7 +2,7 @@
 class Calculations {
 
     static execute(d:{a:number, b:number}, strategy:(x:number, y:number) => number):number {
-        return strategy.apply(d.a, d.b)
+        return strategy.call(this, d.a, d.b)
     }
 }
 
@@ -12,3 +12,16 @@ let substraction1:number = Calculations.execute(digitPair, (a,b) => a - b)
 
 console.log('sum1 ', sum1)
 console.log('substraction1 ', substraction1)
+
+
+//rest parameters
+
+function sum(...digits:number[]):number {
+    return digits.reduce((x, y) => x + y)
+}
+
+let sumOfArray1:number = sum(1, 2, 3)
+let sumOfArray2:number = sum(5, 0, -2)
+console.log('sumOfArray1 ~ ', sumOfArray1)
+console.log('sumOfArray2 ~ ', sumOfArray2)
+//this
